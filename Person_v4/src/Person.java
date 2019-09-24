381e0934934dea955f96ef41587563b423ea4e99
Marton Pentek <51089137+RunnerM@users.noreply.github.com>
2019-09-24 11:15:07 +0200

    24/09/2019

--- Person_v4/src/Person.java
+++ Person_v4/src/Person.java
@@ -1,14 +1,16 @@
+import java.util.Objects;
+
 public class Person
 {
   private Name name;
-  private int age;
+  private MyDate birthday;
   private char gender;
 
 
-  public Person(Name name, int age, char gender)
+  public Person(Name name, MyDate birthday, char gender)
   {
     this.name = name;
-    this.age = age;
+    this.birthday = birthday;
     this.gender = gender;
   }
 
@@ -16,11 +18,13 @@ public class Person
   {
     this.name = name;
     this.gender = gender;
+    this.birthday= new MyDate();
   }
 
   public Person(char gender)
   {
     this.gender = gender;
+    this.birthday= new MyDate();
   }
 
   public Name getName()
@@ -28,9 +32,14 @@ public class Person
     return name;
   }
 
-  public int getAge()
+  public int getAge(){
+    MyDate now = new MyDate();
+    return this.birthday.yearsBetween(now);
+  }
+
+  public MyDate getBirthday()
   {
-    return age;
+    return new MyDate(birthday.getDay(),birthday.getMonth(),birthday.getYear());
   }
 
   public char getGender()
@@ -43,10 +52,10 @@ public class Person
     this.name = name;
   }
 
-  public void setAge(int age)
-  {
-    this.age = age;
-  }
+//  public void setBirthday(MyDate birthday)
+//  {
+//    this.birthday = birthday;
+//  }
 
   public void setGender(char gender)
   {
@@ -55,25 +64,25 @@ public class Person
 
   @Override public String toString()
   {
-    return "Person{" + "name='" + name + '\'' + ", gae=" + age + ", gender="
+    return "Person{" + "name='" + name + '\'' + ", gae=" + birthday + ", gender="
         + gender + '}';
   }
 
   public String getAgeStatus()
   {
-    if (age < 0)
+    if (this.getAge() < 0)
     {
       return "Error";
     }
-    else if (age >= 0 && age <= 12)
+    else if (this.getAge() >= 0 && this.getAge() <= 12)
     {
       return "Child";
     }
-    else if (age > 12 && age <= 19)
+    else if (this.getAge() > 12 && this.getAge() <= 19)
     {
       return "Teenager";
     }
-    else if (age > 19 && age <= 65)
+    else if (this.getAge() > 19 && this.getAge() <= 65)
     {
       return "Adult";
     }
@@ -81,5 +90,15 @@ public class Person
     {
       return "Senior citizen";
     }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Person person = (Person) o;
+    return getGender() == person.getGender() &&
+            getName().equals(person.getName()) &&
+            getBirthday().equals(person.getBirthday());
   }
 }
